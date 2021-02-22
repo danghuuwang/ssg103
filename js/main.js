045c43519ef9a457305f46f41f02fbedb80de3cb
Dang Huu Quang <69068430+danghuuwang@users.noreply.github.com>
2021-02-22 18:06:36 +0700

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -24,3 +24,38 @@ var swiper = new Swiper('.swiper-container', {
       el: '.swiper-pagination',
     },
 });
+
+function popupToggle1() {
+  const popup = document.getElementById('popup1');
+  popup.classList.toggle('active');
+}
+
+function popupToggle2() {
+  const popup = document.getElementById('popup2');
+  popup.classList.toggle('active');
+}
+
+function popupToggle3() {
+  const popup = document.getElementById('popup3');
+  popup.classList.toggle('active');
+}
+
+function popupToggle4() {
+  const popup = document.getElementById('popup4');
+  popup.classList.toggle('active');
+}
+
+function popupToggle5() {
+  const popup = document.getElementById('popup5');
+  popup.classList.toggle('active');
+}
+
+function popupToggle6() {
+  const popup = document.getElementById('popup6');
+  popup.classList.toggle('active');
+}
+
+function popupToggle7() {
+  const popup = document.getElementById('popup7');
+  popup.classList.toggle('active');
+}
